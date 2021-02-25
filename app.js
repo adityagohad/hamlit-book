c29bedcfa28543c49a0a94487bcb3794e518c8c7
adityagohad <adityagohad@gmail.com>
2021-02-25 17:05:14 +0530

    primary push

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+var fs = require('fs');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.end();
+});
+
+app.listen(3000, '0.0.0.0');
